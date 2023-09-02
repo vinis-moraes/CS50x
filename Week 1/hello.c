2ad876d0258dff5fdf818581ddfb45104eb87f45
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2023-09-02 00:01:28 -0300

    Update hello

--- Week 1/hello.c
+++ Week 1/hello.c
@@ -3,6 +3,7 @@
 
 int main(void)
 {
-    string answer = get_string("What's your name? ");
-    printf("hello, world\n");
+    string first = get_string("What's your first name? ");
+    string last = get_string("What's your last name? ");
+    printf("hello, %s %s\n", first, last);
 }
