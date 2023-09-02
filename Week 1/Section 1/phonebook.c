f3e061a8ffaf5b2186093bb6eb7734f9910f1ed2
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2023-09-02 20:20:59 -0300

    Update phonebook.c

--- Week 1/Section 1/phonebook.c
+++ Week 1/Section 1/phonebook.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <cs50.h>
-
+// Main part of this program
 int main(void)
 {
+    // Getting informations
     string name = get_string("Type a name: ");
     int age = get_int("Type an age: ");
     int phone = get_int("Type a phone number: ");
-
+    // Confirming informations
     printf("Name: %s\n", name);
     printf("Age: %i\n", age);
     printf("Phone number: %i\n", phone);
