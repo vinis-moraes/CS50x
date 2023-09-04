ffd955c04f9bfbf9468cd960ddcbbc5b7b58f236
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2023-09-03 22:45:21 -0300

    Add prime

---
+++ Week 1/Practice Problems 1/prime.c
@@ -0,0 +1,44 @@
+#include <cs50.h>
+#include <stdio.h>
+
+bool prime(int number);
+
+int main(void)
+{
+    int min;
+    do
+    {
+        min = get_int("Minimum: ");
+    }
+    while (min < 1);
+
+    int max;
+    do
+    {
+        max = get_int("Maximum: ");
+    } 
+    while (min >= max);
+    
+    for (int i = min; i <= max; i++)
+    {
+        if (prime(i))
+        {
+            printf("%i\n", i);
+        }
+    }
+}
+
+bool prime(int number)
+{
+    // TODO
+    bool is_prime;
+    if (number % 2 != 1)
+    {
+        is_prime = false;
+    } else
+    {
+        is_prime = true;
+    }
+    
+    return is_prime;
+}
